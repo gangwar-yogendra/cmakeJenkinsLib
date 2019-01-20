3d1cb71b66852b74ab8ecf3b4e00bf163e5893fc
gangwar.yogendra <gangwar.yogendra@gmail.com>
2019-01-20 18:53:45 +0530

    Change Lib Name

---
+++ build/CMakeFiles/ZeroMatrixLib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ZeroMatrixLib.dir/src/ZeroMatrix.cpp.o"
+  "libZeroMatrixLib.pdb"
+  "libZeroMatrixLib.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ZeroMatrixLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
